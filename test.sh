d4d4f327a224c418ae1ca648d7c4999df63f41af
Cam Parry <cam.parry@gmail.com>
2017-12-03 13:59:45 +1000

    remove email option from docker login

--- test.sh
+++ test.sh
@@ -58,7 +58,7 @@ for f in "${files[@]}"; do
 done
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+  docker login -u $DOCKER_USER -p $DOCKER_PASS
   if docker build -t wallies/python:nightly-alpine -f python/Dockerfile-nightly-alpine python; then
     docker push wallies/python:nightly-alpine
     echo "Successfully built and pushed"
